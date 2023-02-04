6dc535bcb59d4f82a9bbfa1bfda9f908d935c933
zdrale <nikolazd22@outlook.com>
2023-02-04 16:41:00 +0100

    Added logic for disabeling less button
    --when nothing is added the less button is not functional

--- src/containers/BurgerBuilder.js
+++ src/containers/BurgerBuilder.js
@@ -25,7 +25,22 @@ class BurgerBuilder extends Component {
     this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
   };
 
-  removeIngredientHandler = (type) => {};
+  removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0) {
+      return;
+    }
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = {
+      ...this.state.ingredients,
+    };
+
+    updatedIngredients[type] = updatedCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+  };
   state = {
     ingredients: {
       salad: 0,
@@ -36,10 +51,22 @@ class BurgerBuilder extends Component {
     totalPrice: 2,
   };
   render() {
+    const disableInfo = {
+      ...this.state.ingredients,
+    };
+
+    for (let key in disableInfo) {
+      disableInfo[key] = disableInfo[key] <= 0;
+    }
+
     return (
       <Auxilery>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls ingredientAdded={this.addIngredientHandler} />
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disableInfo}
+        />
       </Auxilery>
     );
   }
