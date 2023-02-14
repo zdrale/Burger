2342a2d08f595b0be478a597d08cffb0de8a37ab
zdrale <nikolazd22@outlook.com>
2023-02-14 13:24:38 +0100

    Added image and style for logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,5 +1,11 @@
 import React from "react";
+import logo from "../../assets/burger.png";
+import classes from "./Logo.css";
 
-const Logo = () => <div>Logo</div>;
+const Logo = () => (
+  <div className={classes.Logo}>
+    <img src={logo} alt="MyBurger"></img>
+  </div>
+);
 
 export default Logo;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,11 @@
 import classes from "./Toolbar.css";
 import React from "react";
+import Logo from "../../Logo/Logo";
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <div>LOGO</div>
+    <Logo />
     <nav>...</nav>
   </header>
 );
