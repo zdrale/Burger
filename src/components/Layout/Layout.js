60deed1629626e78b12c13aaf6c5bae8cf7a3c2e
zdrale <nikolazd22@outlook.com>
2023-02-17 14:32:35 +0100

    Added sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import Auxilery from "../../hoc/Auxilery";
 import React from "react";
 import classes from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const Layout = (props) => (
   <Auxilery>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </Auxilery>
 );
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,8 +2,8 @@ import React from "react";
 import logo from "../../assets/burger.png";
 import classes from "./Logo.css";
 
-const Logo = () => (
-  <div className={classes.Logo}>
+const Logo = (props) => (
+  <div className={classes.Logo} style={{ height: props.height }}>
     <img src={logo} alt="MyBurger"></img>
   </div>
 );
